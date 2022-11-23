5fb19d4e0e24a236839f99961a2d513325614def
Farhan Ahmad <101495798+farhanranjha@users.noreply.github.com>
2022-11-23 11:11:18 +0500

    Add files via upload

---
+++ sqlite.py
@@ -0,0 +1,19 @@
+# Importing Sqlite3 Module
+import sqlite3
+import pandas as pd
+
+con = sqlite3.connect('Task.db')
+
+sql_query = "select * from Items"
+cursor = con.cursor()
+
+df = pd.read_sql_query("SELECT * from Items", con)
+
+for index, row in df.iterrows():
+    if "table" in row['items_name']:
+        row['items_name'] = row['items_name'].replace("table", "desk")
+
+df.to_sql('Task.db', con, schema=None, if_exists='replace', index=True,
+          index_label=None, chunksize=None, dtype=None, method=None)
+
+con.close()
